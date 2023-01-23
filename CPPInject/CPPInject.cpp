baeeeb6662e4e6bab592d44eaca095765a526f01
0xKate <94711905+0xKate@users.noreply.github.com>
2023-01-23 15:04:58 -0800

    Setup Repo and VS

--- CPPInject/CPPInject.cpp
+++ CPPInject/CPPInject.cpp
@@ -1,20 +1,26 @@
-// CPPInject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+   _____ _____  _____ _____       _           _
+  / ____|  __ \|  __ \_   _|     (_)         | |
+ | |    | |__) | |__) || |  _ __  _  ___  ___| |_
+ | |    |  ___/|  ___/ | | | '_ \| |/ _ \/ __| __|
+ | |____| |    | |    _| |_| | | | |  __/ (__| |_
+  \_____|_|    |_|   |_____|_| |_| |\___|\___|\__|
+                                _/ |
+                               |__/
+*/
+/*
+    Copyright (C) 2023  0xKate
 
-#include <iostream>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
