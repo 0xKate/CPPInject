9c3c3bdbf79f58fc9f6377ebcde83bd35a6173f4
0xKate <94711905+0xKate@users.noreply.github.com>
2023-01-24 23:43:34 -0800

    Prep argparse code

--- CPPInject/CPPInject.cpp
+++ CPPInject/CPPInject.cpp
@@ -29,6 +29,7 @@
 #include <windows.h>
 #include <Shlwapi.h>
 #include "x86Injector.h"
+//#include "argparse.hpp"
 
 int main(int argc, char** argv)
 {
@@ -39,6 +40,46 @@ int main(int argc, char** argv)
     printf("CPP Injector is distributed under the GNU GPLv3 license.\n");
     printf("See https://github.com/0xKate/CPPInject/LICENSE.txt for more info.\n");
     printf("Source code @: https://github.com/0xKate/CPPInject\n\n");
+
+    /*
+    argparse::ArgumentParser argParser("CPPInject");
+
+    argParser.add_argument("-v", "--verbose")
+        .default_value(false)
+        .implicit_value(true)
+        .help("Show more detailed logs");
+
+    argParser.add_argument("-d", "--dll")
+        .required()
+        .help("A path to the dll to be injected. ie. \".\file.dll\" or \"D:\\path\\to\\file.dll\"");
+
+    argParser.add_argument("-e", "--exe")
+        .help("A path to the target exe to be launched and injected. ie. \".\file.exe\" or \"D:\\path\\to\\file.exe\"");
+
+    argParser.add_argument("-p", "--pid")
+        .help("The process id of the target process to be injectd.")
+        .scan<'i', int>();
+
+
+
+    std::string dllPath = argParser.get<std::string>("-d");
+    std::cout << "DLL Path: " << dllPath << std::endl;
+
+    std::string exePath = argParser.get<std::string>("-e");
+    std::cout << "EXE Path: " << exePath << std::endl;
+
+    int procID = argParser.get<int>("-p");
+    std::cout << "PID: " << procID << std::endl;
+
+    try {
+        argParser.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << argParser;
+        std::exit(1);
+    }
+    */
 
     if (argc == 3)
     {
@@ -52,8 +93,7 @@ int main(int argc, char** argv)
             return EXIT_FAILURE;
         }
     }
-    else
-    {
+    else {
         fprintf(stderr, "Invalid number of arguments!\n");
         fprintf(stderr, "Argument1: DllPath\nArgument2: PathToTargetExe\n");
         return EXIT_FAILURE;
